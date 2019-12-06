6d60b236eddccef0237855e6b6e5ba093a464706
Hélder Bacanhim <43825339+phn0m@users.noreply.github.com>
2019-12-06 13:32:53 +0000

    165

--- projectAtlas/backend/routes/pessoas.js
+++ projectAtlas/backend/routes/pessoas.js
@@ -11,4 +11,36 @@ router.get('/', function (req, res) {
         res.send(data);
     });
 });
+router.post("/",function (req, res) {
+    var obj = req.body;
+    var model = new pessoa(obj);
+    model.save(function (err) {
+        if (err) {
+            res.send("error");
+            return;
+        }
+        res.send("created");
+    })
+});
+router.get("/:id", function (req, res) {
+    var id = req.params.id;
+    pessoa.find({_id: id}, function (err, data) {
+        if (err) {
+            res.send("error");
+            return;
+        }
+        res.send(data[0]);
+    })
+});
+router.post("/:id",function (req, res) {
+    var id = req.params.id;
+    var obj = req.body;
+    pessoa.findByIdAndUpdate(id, {nome: obj.nome, apelido: obj.apelido,numTelemovel:obj.numTelemovel,data_de_nascimento:obj.data_de_nascimento}, function (err) {
+        if (err) {
+            res.send("error");
+            return;
+        }
+        res.send("updated");
+    });
+});
 module.exports = router;
